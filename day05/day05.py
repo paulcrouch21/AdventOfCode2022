a2e7314ff3bb067f1f427e3ada887743390b1adc
Paul Crouch Jr <paul.crouch99@yahoo.com>
2022-12-05 13:18:37 -0600

    answer to day 05 part 02 - only works when the print line for each part is commented out...

--- day05/day05.py
+++ day05/day05.py
@@ -18,7 +18,8 @@ def main():
      #make the bottom of the stack index 0 - reverse the string
      stacks = [stack[::-1] for stack in stacks]
 
-     print(f'The crates that end up on top of each stack is {part01(stacks, cratesAndDirections[1])}')
+     #print(f'The crates that end up on top of each stack is {part01(stacks, cratesAndDirections[1])} [part 01]')
+     #print(f'The crates that end up on top of each stack is {part02(stacks, cratesAndDirections[1])} [part 02]')
 
 def part01(crateStacks, directions):
 
@@ -32,6 +33,21 @@ def part01(crateStacks, directions):
           for _ in range(n):
                pop = crateStacks[source].pop()
                crateStacks[destination].append(pop)
+
+     topOfStacks = [stack[-1] for stack in crateStacks]
+
+     return ''.join(topOfStacks)
+
+def part02(crateStacks, directions):
+
+     for line in directions.split('\n'):
+          tokens = line.split(' ')
+          n, source, destination = map(int, [tokens[1], tokens[3], tokens[5]])
+          source -= 1
+          destination -= 1
+
+          crateStacks[destination].extend(crateStacks[source][-n:])
+          crateStacks[source] = crateStacks[source][:-n]
 
      topOfStacks = [stack[-1] for stack in crateStacks]
 
