91da9461b2a189421c547abffa446ceb26a8a67d
Paul Crouch Jr <paul.crouch99@yahoo.com>
2022-12-05 12:55:03 -0600

    answer for day05 part 01 (with assistance)

---
+++ day05/day05.py
@@ -0,0 +1,42 @@
+import sys
+
+def main():
+     #reads the input file and separates into two parts
+     with open(sys.argv[1]) as myFile:
+          cratesAndDirections = myFile.read()[:-1].split('\n\n')
+          crates = cratesAndDirections[0].split('\n')
+          #list containing sublists that represents every stack
+          stacks = [[] for _ in range(len(crates))]
+          
+          for i in range(len(crates)):
+               line = crates[i]
+               letters = line[1::4]
+               for s in range(len(letters)):
+                    if letters[s] != ' ':
+                         stacks[s].append(letters[s])
+
+     #make the bottom of the stack index 0 - reverse the string
+     stacks = [stack[::-1] for stack in stacks]
+
+     print(f'The crates that end up on top of each stack is {part01(stacks, cratesAndDirections[1])}')
+
+def part01(crateStacks, directions):
+
+     for line in directions.split('\n'):
+          tokens = line.split(' ')
+          n, source, destination = map(int, [tokens[1], tokens[3], tokens[5]])
+          source -= 1
+          destination -= 1
+
+          #moves the crate to the desired location
+          for _ in range(n):
+               pop = crateStacks[source].pop()
+               crateStacks[destination].append(pop)
+
+     topOfStacks = [stack[-1] for stack in crateStacks]
+
+     return ''.join(topOfStacks)
+
+
+if __name__ == '__main__':
+     main()
