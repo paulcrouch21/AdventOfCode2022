c1149ec0a63f7e6cf8d92659cf2d1a296cfdac4b
Paul Crouch Jr <paul.crouch99@yahoo.com>
2022-12-06 07:23:46 -0600

    answer to day06 part 02

--- day06/day06.py
+++ day06/day06.py
@@ -6,6 +6,7 @@ def main():
           lines = [line.strip() for line in lines]
 
      print(f'There are {part01(lines[0])} characters that need to be processed before the first start-of-packet maker is detected. [part 01]')
+     print(f'There are {part02(lines[0])} characters that need to be processed before the first start-of-packet maker is detected. [part 02]')
 
 def part01(data):
 
@@ -15,6 +16,17 @@ def part01(data):
           if len(inputSet) == 4:
                return counter + 4
           counter += 1
+
+def part02(data):
+
+     counter = 0
+     while counter < len(data):
+          inputSet = set(data[counter:counter + 14])
+          if len(inputSet) == 14:
+               return counter + 14
+          counter += 1
+
+     return
 
 
 if __name__ == '__main__':
